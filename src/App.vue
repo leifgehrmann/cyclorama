2d902e66007102f87094a4388affb33f2e6534ef
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-01-19 23:09:06 +0000

    Added tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,14 @@
 <script setup lang="ts">
 import Cyclorama from './components/Cyclorama.vue'
+import Joystick from './components/Joystick.vue';
 </script>
 
 <template>
-  <Cyclorama />
+  <div>
+    <Cyclorama />
+    <Joystick />
+  </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
