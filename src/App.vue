597a6ace596bcaf8131d9bf6d7d283e1b0d30e1d
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-01-27 10:10:11 +0000

    mouse events outside of joystick

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,42 @@
 import Cyclorama from './components/Cyclorama.vue'
 import Joystick from './components/Joystick.vue';
 import Controls from "./components/Controls.vue";
+import {onMounted} from "vue";
+
+type Position = [number, number];
+
+let mouseDown: null | Position = null;
+
+onMounted(() => {
+  window.addEventListener('mousedown', (e) => {
+    mouseDown = [e.clientX, e.clientY];
+  })
+
+  window.addEventListener('mousemove', (e) => {
+    if (mouseDown === null) {
+      return
+    }
+    console.log([e.clientX - mouseDown[0], e.clientY - mouseDown[1]]);
+  })
+
+  function clearMouse() {
+    mouseDown = null;
+  }
+
+  window.addEventListener('mouseleave', clearMouse)
+  window.addEventListener('mouseup', clearMouse)
+  window.addEventListener('mouseout', clearMouse)
+})
+
+function joystickUpdate(e) {
+}
+
 </script>
 
 <template>
   <div class="touch-none">
-    <Cyclorama />
-    <Joystick />
+    <Cyclorama/>
+    <Joystick @controlUpdate="joystickUpdate"/>
     <div
         class="absolute top-0 right-0"
     >
