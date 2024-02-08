f14e49d1e2dac6f817c650cc6d83f654b0dc6bd2
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-02-08 23:44:32 +0000

    Writing some notes about the st giles panorama

---
+++ src/components/explanations/EdinburghStGiles.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import Attribution from "./Attribution.vue";
+</script>
+
+<template>
+  <h1 class="text-center text-xl leading-4"><span class="text-3xl font-bold">EDINBURGH</span><br><em>from the crown of</em><br><span class="text-2xl font-bold" style="font-variant: small-caps">St. Giles'</span><br><span class="text-2xl font-bold leading-[0rem]" style="font-variant: small-caps">Cathedral</span></h1>
+  <p class="text-center text-sm"><small>Attributed to</small><br>HENRY ASTON BARKER</p>
+  <p class="text-center text-xl">1807*</p>
+  <hr>
+  <p>* I was unable to find any evidence of when this panorama was created. But it is clear that it could not have been created by Robert Barker because he died in 1806, and <a href="https://en.wikipedia.org/wiki/Nelson_Monument,_Edinburgh">Nelson Monument</a> was constructed in 1807.</p>
+  <Attribution
+      identifier="ADO-2016-0006"
+      license="Public Domain"
+      url="https://archives.collections.ed.ac.uk/repositories/2/digital_objects/100"
+      entity="the University of Edinburgh"
+  />
+  <img src="/public/BARKER_EDINBURGH_500.jpg">
+</template>
+
+<style scoped>
+
+</style>
