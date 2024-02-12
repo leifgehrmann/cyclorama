c12dfdb5cf870a22cdb2f0f558a63ddbf9cc8317
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-02-12 22:02:01 +0000

    Updated details about London panorama (No longer using the government archive's scans)

--- src/components/explanations/LondonBarker.vue
+++ src/components/explanations/LondonBarker.vue
@@ -5,15 +5,21 @@ import Attribution from "./Attribution.vue";
 
 <template>
   <h1 class="text-center text-xl leading-4"><span class="text-6xl font-black">LONDON</span><br>from the Roof of Albion Mills</h1>
-  <p class="text-center text-sm"><small>By</small> Henry Aston Barker</p>
-  <p class="text-center text-xl">1792</p>
-  Note, a key exists online, perhaps that can be linked here.
+  <p class="text-center text-sm">
+    <small>Sketches by</small> Henry Aston Barker<br>
+    <small>Painted by</small> Robert Barker<br>
+    <small>Aquatint by</small> Frederick Birnie
+  </p>
+  <p class="text-center text-xl">1792-1793</p>
+  <p>A key of the panorama can be found at the <a href="https://www.britishmuseum.org/collection/object/P_1880-1113-5509-1">British Museum (Museum Number: 1880,1113.5509.1)</a></p>
+
   <hr>
+  <p>Original images were downloaded from the Yale Center for British Art. The images were then trimmed and edited by Leif Gehrmann. The edited version is licensed to the public domain to match the original.</p>
   <Attribution
-      identifier="GAC number 1222"
+      identifier="orbis:205530"
       license="Public Domain"
-      url="https://artcollection.culture.gov.uk/artwork/1222/"
-      entity="the Government Art Collection"
+      url="https://collections.britishart.yale.edu/catalog/orbis:205530"
+      entity="Yale Center for British Art"
   />
   <img src="/public/blondon.jpg">
 </template>
