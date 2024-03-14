a8509bcb4a9b9d41d92ddbecebaff639aa5da4c0
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-03-14 23:37:50 +0000

    Added main page, and ability to select a panorarama (currently only one selection working)

---
+++ src/components/CycloramaScreen.vue
@@ -0,0 +1,548 @@
+<script setup lang="ts">
+import Cyclorama from './Cyclorama.vue'
+import Joystick from './Joystick.vue';
+import Controls from "./Controls.vue";
+import {onMounted, ref} from "vue";
+import {ControlState} from "../utils/types.ts";
+import getTouchById, {getAngularDifferenceFromPointers, getDistanceBetweenPointers} from "../utils/pointerUtils.ts";
+import * as THREE from 'three';
+import Info from "./Info.vue";
+import Loading from "./Loading.vue";
+
+const controlState = ref({
+  sagittalAcc: 0,
+  frontalAcc: 0,
+  yawAcc: 0,
+  pitchAcc: 0,
+  yawVel: null,
+  pitchVel: null,
+  yawVelDeceleration: 0.8,
+  pitchVelDeceleration: 0.8,
+  zoomAcc: 0,
+  heightAcc: 0,
+  rollAcc: 0,
+  boundaryBreak: false,
+} as ControlState)
+const camera = ref(new THREE.PerspectiveCamera());
+const interactive = ref(null as null | HTMLDivElement)
+const hiddenControls = ref(false)
+const showInfo = ref(false)
+const progress = ref(0)
+
+camera.value.aspect = window.innerWidth / window.innerHeight;
+camera.value.updateProjectionMatrix()
+
+let pointer2ndPrev: null | Touch | MouseEvent = null;
+let pointerPrev: null | Touch | MouseEvent = null;
+let touchId: number | null = null;
+let joystickMax = 0.015;
+let joystickMinThreshold = 0.1;
+let controlZoomMax = 0.005;
+let keyboardRotationAcc = 0.0015;
+let keyboardRotationAccSlow = 0.0001;
+let keyboardRotationAccFast = 0.020;
+let keyboardFovAcc = 0.01;
+let keyboardHeightAcc = 0.01;
+let keyboardWalkingAcc = 0.01;
+let keyboardWalkingAccFast = 0.02;
+let keyboardWalkingAccSlow = 0.001;
+let keyboardShift = false;
+let keyboardAlt = false;
+let keyboardW = false;
+let keyboardA = false;
+let keyboardS = false;
+let keyboardD = false;
+let keyboardUp = false;
+let keyboardDown = false;
+let keyboardLeft = false;
+let keyboardRight = false;
+let keyboardPlus = false;
+let keyboardMinus = false;
+let keyboardI = false;
+let keyboardK = false;
+let keyboardJ = false;
+let keyboardL = false;
+let keyboardRotationDeceleration = 0.8;
+let pointerRotationDeceleration = 0.95;
+
+function selectNormSlowFast(
+    fastSelected: boolean,
+    slowSelected: boolean,
+    normSpeed: number,
+    slowSpeed: number,
+    fastSpeed: number,
+) {
+  if (fastSelected && !slowSelected) {
+    return fastSpeed;
+  } else if (slowSelected && !fastSelected) {
+    return slowSpeed;
+  } else {
+    return normSpeed;
+  }
+}
+
+function normaliseControlStateFromKeyboardState() {
+  // compute direction from keyboard. Keyboard overrides joystick controls.
+  if (keyboardW || keyboardA || keyboardS || keyboardD) {
+    controlState.value.sagittalAcc = 0;
+    controlState.value.frontalAcc = 0;
+    if (keyboardW) {
+      controlState.value.sagittalAcc += selectNormSlowFast(
+          keyboardShift,
+          keyboardAlt,
+          keyboardWalkingAcc,
+          keyboardWalkingAccSlow,
+          keyboardWalkingAccFast
+      );
+    }
+    if (keyboardA) {
+      controlState.value.frontalAcc -= selectNormSlowFast(
+          keyboardShift,
+          keyboardAlt,
+          keyboardWalkingAcc,
+          keyboardWalkingAccSlow,
+          keyboardWalkingAccFast
+      );
+    }
+    if (keyboardS) {
+      controlState.value.sagittalAcc -= selectNormSlowFast(
+          keyboardShift,
+          keyboardAlt,
+          keyboardWalkingAcc,
+          keyboardWalkingAccSlow,
+          keyboardWalkingAccFast
+      );
+    }
+    if (keyboardD) {
+      controlState.value.frontalAcc += selectNormSlowFast(
+          keyboardShift,
+          keyboardAlt,
+          keyboardWalkingAcc,
+          keyboardWalkingAccSlow,
+          keyboardWalkingAccFast
+      );
+    }
+    if (controlState.value.frontalAcc !== 0 && controlState.value.sagittalAcc !== 0 ) {
+      controlState.value.sagittalAcc *= 1 / Math.sqrt(2);
+      controlState.value.frontalAcc *= 1 / Math.sqrt(2);
+    }
+  }
+  if (keyboardLeft || keyboardRight) {
+    controlState.value.yawAcc = 0
+    if (keyboardLeft) {
+      controlState.value.yawAcc -= selectNormSlowFast(
+        keyboardShift,
+        keyboardAlt,
+        keyboardRotationAcc,
+        keyboardRotationAccSlow,
+        keyboardRotationAccFast
+      );
+    }
+    if (keyboardRight) {
+      controlState.value.yawAcc += selectNormSlowFast(
+        keyboardShift,
+        keyboardAlt,
+        keyboardRotationAcc,
+        keyboardRotationAccSlow,
+        keyboardRotationAccFast
+      );
+    }
+  }
+  if (keyboardUp || keyboardDown) {
+    controlState.value.pitchAcc = 0
+    if (keyboardUp) {
+      controlState.value.pitchAcc -= selectNormSlowFast(
+          keyboardShift,
+          keyboardAlt,
+          keyboardRotationAcc,
+          keyboardRotationAccSlow,
+          keyboardRotationAccFast
+      );
+    }
+    if (keyboardDown) {
+      controlState.value.pitchAcc += selectNormSlowFast(
+          keyboardShift,
+          keyboardAlt,
+          keyboardRotationAcc,
+          keyboardRotationAccSlow,
+          keyboardRotationAccFast
+      );
+    }
+  }
+
+  if (keyboardMinus || keyboardPlus) {
+    controlState.value.zoomAcc = 0;
+    if (keyboardMinus) {
+      controlState.value.zoomAcc -= keyboardFovAcc;
+    }
+    if (keyboardPlus) {
+      controlState.value.zoomAcc += keyboardFovAcc;
+    }
+  }
+
+  if (keyboardI || keyboardK) {
+    controlState.value.heightAcc = 0;
+    if (keyboardK) {
+      controlState.value.heightAcc -= keyboardHeightAcc;
+    }
+    if (keyboardI) {
+      controlState.value.heightAcc += keyboardHeightAcc;
+    }
+  }
+
+  if (keyboardJ || keyboardL) {
+    controlState.value.rollAcc = 0;
+    if (keyboardJ) {
+      controlState.value.rollAcc -= keyboardRotationAcc;
+    }
+    if (keyboardL) {
+      controlState.value.rollAcc += keyboardRotationAcc;
+    }
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('mousedown', (e) => {
+    controlState.value.yawVel = 0
+    controlState.value.pitchVel = 0
+    controlState.value.yawAcc = 0;
+    controlState.value.pitchAcc = 0;
+    controlState.value.yawVelDeceleration = pointerRotationDeceleration;
+    controlState.value.pitchVelDeceleration = pointerRotationDeceleration;
+    pointerPrev = e;
+  })
+
+  window.addEventListener('touchstart', (e) => {
+    if (touchId !== null) {
+      return;
+    }
+    e.preventDefault();
+    let touch = e.changedTouches[0];
+    touchId = touch.identifier;
+    controlState.value.yawVel = 0
+    controlState.value.pitchVel = 0
+    controlState.value.yawAcc = 0;
+    controlState.value.pitchAcc = 0;
+    controlState.value.yawVelDeceleration = pointerRotationDeceleration;
+    controlState.value.pitchVelDeceleration = pointerRotationDeceleration;
+    pointer2ndPrev = null;
+    pointerPrev = touch;
+  })
+
+  window.addEventListener('mousemove', (e) => {
+    if (pointerPrev === null || interactive.value === null) {
+      return
+    }
+    const {yaw, pitch} = getAngularDifferenceFromPointers(
+        camera.value,
+        e,
+        pointerPrev,
+        interactive.value
+    )
+    controlState.value.yawVel = (controlState.value.yawVel ?? 0) + yaw;
+    controlState.value.pitchVel = (controlState.value.pitchVel ?? 0) + pitch;
+    pointer2ndPrev = pointerPrev;
+    pointerPrev = e;
+    requestAnimationFrame(() => {
+      controlState.value.yawVel = null;
+      controlState.value.pitchVel = null;
+    })
+  })
+
+  window.addEventListener('touchmove', (e) => {
+    if (touchId === null || pointerPrev === null || interactive.value === null) {
+      return;
+    }
+    const touch = getTouchById(e.changedTouches, touchId)
+    if (touch === null) {
+      return;
+    }
+    const {yaw, pitch} = getAngularDifferenceFromPointers(
+        camera.value,
+        touch,
+        pointerPrev,
+        interactive.value
+    )
+    controlState.value.yawVel = (controlState.value.yawVel ?? 0) + yaw;
+    controlState.value.pitchVel = (controlState.value.pitchVel ?? 0) + pitch;
+    pointer2ndPrev = pointerPrev;
+    pointerPrev = touch;
+    requestAnimationFrame(() => {
+      controlState.value.yawVel = null;
+      controlState.value.pitchVel = null;
+    })
+  })
+
+  function clearMouse(e: MouseEvent) {
+    if (pointer2ndPrev === null || interactive.value === null) {
+      pointerPrev = null;
+      return;
+    }
+    const {yaw, pitch} = getAngularDifferenceFromPointers(
+        camera.value,
+        e,
+        pointer2ndPrev,
+        interactive.value
+    )
+    // Only perform momentum if the difference is big enough.
+    if (getDistanceBetweenPointers(e, pointer2ndPrev) > 2) {
+      controlState.value.yawVel = null
+      controlState.value.pitchVel = null
+      controlState.value.yawAcc = yaw;
+      controlState.value.pitchAcc = pitch;
+    }
+    pointer2ndPrev = null;
+    pointerPrev = null;
+    requestAnimationFrame(() => {
+      controlState.value.yawAcc = 0;
+      controlState.value.pitchAcc = 0;
+    })
+  }
+
+  function clearTouch(e: TouchEvent) {
+    if (touchId === null || interactive.value === null) {
+      return;
+    }
+    const touch = getTouchById(e.changedTouches, touchId)
+    if (touch === null) {
+      // This means the touchend has not yet fired for the touch that is
+      // actively controlling the view.
+      return;
+    }
+    if (pointer2ndPrev === null) {
+      pointerPrev = null;
+      touchId = null;
+      return
+    }
+    const {yaw, pitch} = getAngularDifferenceFromPointers(
+        camera.value,
+        touch,
+        pointer2ndPrev,
+        interactive.value
+    )
+    // Only perform momentum if the difference is big enough.
+    if (getDistanceBetweenPointers(touch, pointer2ndPrev) > 2) {
+      controlState.value.yawVel = null
+      controlState.value.pitchVel = null
+      controlState.value.yawAcc = yaw;
+      controlState.value.pitchAcc = pitch;
+    }
+    pointer2ndPrev = null;
+    pointerPrev = null;
+    touchId = null;
+    requestAnimationFrame(() => {
+      controlState.value.yawAcc = 0;
+      controlState.value.pitchAcc = 0;
+    })
+  }
+
+  window.addEventListener('mouseleave', clearMouse)
+  window.addEventListener('mouseup', clearMouse)
+  window.addEventListener('mouseout', clearMouse)
+  window.addEventListener('touchend', clearTouch)
+  window.addEventListener('touchcancel', clearTouch)
+
+  window.addEventListener('keydown', (event) => {
+    if (
+        event.code === 'ArrowLeft' ||
+        event.code === 'ArrowRight' ||
+        event.code === 'ArrowDown' ||
+        event.code === 'ArrowUp'
+    ) {
+      controlState.value.yawVel = null;
+      controlState.value.pitchVel = null;
+      controlState.value.yawVelDeceleration = keyboardRotationDeceleration;
+      controlState.value.pitchVelDeceleration = keyboardRotationDeceleration;
+    }
+
+    if (event.code === 'ArrowLeft') {
+      event.preventDefault();
+      keyboardLeft = true;
+    } else if (event.code === 'ArrowRight') {
+      event.preventDefault();
+      keyboardRight = true;
+    } else if (event.code === 'ArrowDown') {
+      event.preventDefault();
+      keyboardDown = true;
+    } else if (event.code === 'ArrowUp') {
+      event.preventDefault();
+      keyboardUp = true;
+    } else if (event.code === 'Minus') {
+      keyboardMinus = true;
+      controlState.value.zoomAcc = -keyboardFovAcc;
+    } else if (event.code === 'Equal') {
+      keyboardPlus = true;
+      controlState.value.zoomAcc = keyboardFovAcc;
+    } else if (event.code === 'KeyI') {
+      keyboardI = true;
+      controlState.value.heightAcc = keyboardHeightAcc;
+    } else if (event.code === 'KeyK') {
+      keyboardK = true;
+      controlState.value.heightAcc = -keyboardHeightAcc;
+    } else if (event.code === 'KeyJ') {
+      keyboardJ = true;
+      controlState.value.rollAcc = -keyboardRotationAcc;
+    } else if (event.code === 'KeyL') {
+      keyboardL = true;
+      controlState.value.rollAcc = keyboardRotationAcc;
+    } else if (event.code === 'KeyB') {
+      controlState.value.boundaryBreak = !controlState.value.boundaryBreak;
+    } else if (event.code === 'KeyH') {
+      hiddenControls.value = !hiddenControls.value;
+    } else if (event.code === 'KeyW') {
+      keyboardW = true;
+    } else if (event.code === 'KeyA') {
+      keyboardA = true;
+    } else if (event.code === 'KeyS') {
+      keyboardS = true;
+    } else if (event.code === 'KeyD') {
+      keyboardD = true;
+    }
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+      keyboardShift = true;
+    }
+    if (event.code === 'AltLeft' || event.code === 'AltRight') {
+      keyboardAlt = true;
+    }
+    normaliseControlStateFromKeyboardState();
+  })
+
+  window.addEventListener('keyup', (event) => {
+    if (event.code === 'ArrowLeft') {
+      controlState.value.yawAcc = 0;
+      keyboardLeft = false;
+    } else if (event.code === 'ArrowRight') {
+      controlState.value.yawAcc = 0;
+      keyboardRight = false;
+    } else if (event.code === 'ArrowDown') {
+      controlState.value.pitchAcc = 0;
+      keyboardDown = false;
+    } else if (event.code === 'ArrowUp') {
+      controlState.value.pitchAcc = 0;
+      keyboardUp = false;
+    } else if (event.code === 'Minus') {
+      controlState.value.zoomAcc = 0;
+      keyboardMinus = false;
+    } else if (event.code === 'Equal') {
+      controlState.value.zoomAcc = 0;
+      keyboardPlus = false;
+    } else if (event.code === 'KeyI') {
+      controlState.value.heightAcc = 0;
+      keyboardI = false;
+    } else if (event.code === 'KeyK') {
+      controlState.value.heightAcc = 0;
+      keyboardK = false;
+    } else if (event.code === 'KeyJ') {
+      controlState.value.rollAcc = 0;
+      keyboardJ = false;
+    } else if (event.code === 'KeyL') {
+      controlState.value.rollAcc = 0;
+      keyboardL = false;
+    } else if (event.code === 'KeyW') {
+      keyboardW = false;
+      controlState.value.sagittalAcc = 0;
+      controlState.value.frontalAcc = 0;
+    } else if (event.code === 'KeyA') {
+      keyboardA = false;
+      controlState.value.sagittalAcc = 0;
+      controlState.value.frontalAcc = 0;
+    } else if (event.code === 'KeyS') {
+      keyboardS = false;
+      controlState.value.sagittalAcc = 0;
+      controlState.value.frontalAcc = 0;
+    } else if (event.code === 'KeyD') {
+      keyboardD = false;
+      controlState.value.sagittalAcc = 0;
+      controlState.value.frontalAcc = 0;
+    }
+    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+      keyboardShift = false;
+    }
+    if (event.code === 'AltLeft' || event.code === 'AltRight') {
+      keyboardAlt = false;
+    }
+    normaliseControlStateFromKeyboardState();
+  })
+})
+
+function joystickUpdate(e: [number, number]) {
+  if (Math.abs(e[1]) > joystickMinThreshold) {
+    let sign = Math.sign(e[1])
+    controlState.value.sagittalAcc = (-(e[1] + sign * joystickMinThreshold)) / (1 + joystickMinThreshold) * joystickMax
+  } else {
+    controlState.value.sagittalAcc = 0;
+  }
+  if (Math.abs(e[0]) > joystickMinThreshold) {
+    let sign = Math.sign(e[0])
+    controlState.value.frontalAcc = (e[0] + sign * joystickMinThreshold) / (1 + joystickMinThreshold) * joystickMax
+  } else {
+    controlState.value.frontalAcc = 0;
+  }
+}
+
+function controlZoomUpdate(e: [number]) {
+  controlState.value.zoomAcc = e[0] * controlZoomMax;
+}
+
+function toggleInfo() {
+  showInfo.value = !showInfo.value;
+}
+
+function updateProgress (e: [number]) {
+  progress.value = e[0];
+}
+
+</script>
+
+<template>
+  <div
+      class="absolute w-screen h-screen max-h-screen min-h-screen bg-black z-10 pointer-events-none flex items-center justify-center flex-col gap-5"
+      :class="{hidden: progress === 1}"
+  >
+    <Loading :progress="progress" />
+  </div>
+  <div
+      class="grid w-screen h-screen max-h-screen min-h-screen grid-rows-1 grid-cols-1"
+      :class="{
+    'md:grid-rows-2 md:grid-cols-1 2xl:grid-rows-1 2xl:grid-cols-[1fr_minmax(20rem,_33%)]': showInfo,
+      }"
+      style="height: 100dvh; min-height: 100dvh; max-height: 100dvh;">
+    <div ref="interactive" class="touch-none relative md:block"
+         :class="{ hidden: showInfo }"
+    >
+      <Cyclorama
+          :camera="camera"
+          :controlState="controlState"
+          @progressUpdate="updateProgress"
+      />
+      <div
+          class="absolute bottom-0 left-0 pointer-events-none"
+          :class="{ hidden: hiddenControls }"
+      >
+        <div class="p-6">
+          <Joystick @controlUpdate="joystickUpdate"/>
+        </div>
+      </div>
+      <div
+          class="absolute top-0 right-0 pointer-events-none"
+          :class="{ hidden: hiddenControls }"
+      >
+        <div class="p-4 pointer-events-auto">
+          <Controls
+              :show-info="showInfo"
+              @toggle-info="toggleInfo"
+              @zoom-update="controlZoomUpdate"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="overflow-y-scroll overflow-x-hidden"
+         :class="{ hidden: !showInfo, 'md:block': showInfo }"
+    >
+      <Info @toggle-info="toggleInfo"/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/SelectionScreen.vue
@@ -0,0 +1,96 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="screen">
+    <h1 class="italic text-3xl font-bold">Cycloramas</h1>
+    <p class="mb-10">A collection of panoramas from the 1800s and a virtual recreation of the cyclorama, a building designed to exhibit massive 360-degree paintings, surrounding the viewer. For more information, read <a href="https://leifgehrmann.com"><em>Virtual Reality in the&nbsp;1800s</em></a> by Leif Gehrmann.</p>
+    <h2>1. Panoramas by the Barkers</h2>
+    <p>Panoramas from Robert Barker, Henry Aston Barker, or any other panoramas exhibited at Leicester Square or the Strand in London.</p>
+    <ul>
+      <li style="background-image: url('/public/barker.jpg');"><a href="./?view=edinburgh">
+        View of Edinburgh from Calton Hill<br>
+        <span class="text-xs">1789- (Watercolour from 1792)</span>
+      </a></li>
+      <li style="background-image: url('/public/blondon.jpg')"><a href="./?view=edinburgh">
+        London from the Roof of Albion Mills<br>
+        <span class="text-xs">17??-17?? (Aquatint from 1792-1793)</span>
+      </a></li>
+      <li style="background-image: url('/public/1982,U.3982-panorama.jpg')"><a href="./?view=edinburgh">
+        View of Treport, the surrounding Country, and Chateau d'Eu<br>
+        <span class="text-xs">19??</span>
+      </a></li>
+    </ul>
+    <h2>2. Thomas Hornor's Colosseum</h2>
+    <p>Panoramas related to Thomas Hornor's panorama taken from the top of St. Paul Cathedral, including a modern view of London for comparison.</p>
+    <ul>
+      <li style="background-image: url('/public/1880,1113.1213.jpg');"><a href="./?view=edinburgh">
+        View of Edinburgh from Calton Hill<br>
+        <span class="text-xs">1789- (Watercolour from 1792)</span>
+      </a></li>
+      <li style="background-image: url('/public/1880,1113.1214.jpg')"><a href="./?view=edinburgh">
+        London from the Roof of Albion Mills<br>
+        <span class="text-xs">17??-17?? (Aquatint from 1792-1793)</span>
+      </a></li>
+      <li style="background-image: url('/public/london.jpg')"><a href="./?view=edinburgh">
+        Panoramic view of London taken from the Golden Gallery of St Paulʼs Cathedral<br>
+        <span class="text-xs">2007, Photo by David Iliff</span>
+      </a></li>
+    </ul>
+    <h2>Credits</h2>
+    <p class="mb-16">
+      Compilation and recreation of panoramas by <a href="https://leifgehrmann.com">Leif Gehrmann</a>. Source code for this website is available on <a href="https://github.com/leifgehrmann/cyclorama">GitHub</a>. For attribution of the original panoramas, please press the <img src="../assets/show-info-inactive.svg" alt="info" class="inline opacity-60" style="height: 1ic"> button on each page.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+#screen * {
+  -webkit-touch-callout: default;
+  -webkit-user-select: text;
+  -moz-user-select: text;
+  -ms-user-select: text;
+  user-select: text;
+}
+
+ul {
+  @apply mx-auto my-10 max-w-96 justify-center items-center;
+}
+
+li {
+  @apply bg-cover;
+}
+
+li + li {
+  @apply mt-4;
+}
+
+li a {
+  @apply block py-10 px-10 w-full cursor-pointer bg-black/70 transition-colors ease-in;
+}
+
+li a:hover {
+  @apply bg-black/85;
+}
+
+h1, h2, p, a {
+  @apply text-center text-white/75 font-serif;
+}
+
+h1 {
+  @apply my-10;
+}
+
+h2 {
+  @apply text-xl;
+}
+
+p {
+  @apply text-sm text-white/60 max-w-64 mx-auto;
+}
+
+p a {
+  @apply text-indigo-400;
+}
+
+</style>
