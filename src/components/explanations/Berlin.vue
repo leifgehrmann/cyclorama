b9745dddbc2cec80e3b756c038b602ce4da578e5
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-03-19 20:07:52 +0000

    Added explanation for Berlin

--- src/components/explanations/Berlin.vue
+++ src/components/explanations/Berlin.vue
@@ -13,7 +13,55 @@ import Attribution from "./Attribution.vue";
   <hr>
   <p class="text-center">Exhibited in 1814 at the Strand Panorama in London. The dimensions of this cyclorama are unknown, so the dimensions of the Lower Circle at the Leicester Square Panorama (which at the time was a competitor to the Strand Panorama) is used instead.</p>
   <hr>
-  <p>TODO</p>
+  <h1 class="text-center leading-3">
+    <span class="uppercase text-lg">Explanation</span><br>
+    <span class="uppercase text-xs">of the</span><br>
+    <span class="uppercase italic text-lg">view of</span><br>
+    <span class="uppercase text-6xl tracking-widest font-bold">Berlin,</span><br>
+    <span class="uppercase text-xs">now exhibiting in the</span><br>
+    <span class="uppercase italic text-4xl font-medium">Panorama,</span><br>
+    <span class="uppercase text-3xl font-medium">Strand.</span><br>
+  </h1>
+  <p class="text-center mb-[-1rem]">~~~~~</p>
+  <p class="text-center mb-[-1rem] italic">Open from Ten till Dusk.</p>
+  <p class="text-center mb-[-1rem]">~~~~~</p>
+  <p class="text-center text-xl">1814.</p>
+  <hr>
+  <ol>
+    <li>Royal Palace.</li>
+    <li>St. Peter's Church.</li>
+    <li>Turret where the large Silver Crown was formerly kept.</li>
+    <li>Werder Church.</li>
+    <li>German Church.</li>
+    <li>National Theatre.</li>
+    <li>French Church.</li>
+    <li>Catholic Church.</li>
+    <li>Trinity Church.</li>
+    <li>City Governor's Palace.</li>
+    <li>King's small Palace.</li>
+    <li>Opera House.</li>
+    <li>King's Library.</li>
+    <li>Arsenal.</li>
+    <li>Prince Henry's Palace.</li>
+    <li>A Hotel, called the Parliament of England.</li>
+    <li>Artillery Barracks.</li>
+    <li>Orange Bridge.</li>
+    <li>New Custom House.</li>
+    <li>Exchange.</li>
+    <li>Frederick's Bridge.</li>
+    <li>Spandauer Church.</li>
+    <li>The House of Itzig the Banker.</li>
+    <li>Garrison Church.</li>
+    <li>Gymnasium.</li>
+    <li>St. Mary's Church.</li>
+    <li>Military Academy.</li>
+    <li>Parish Church.</li>
+    <li>St. Nicholas Church.</li>
+    <li>Long Bridge.</li>
+    <li>Remains of the Old Palace.</li>
+    <li>King's Apothecary's House.</li>
+  </ol>
+  <p><span style="font-variant: small-caps">J. Adlard,</span> Printer, 23, Bartholomew Close.</p>
 </template>
 
 <style scoped>
