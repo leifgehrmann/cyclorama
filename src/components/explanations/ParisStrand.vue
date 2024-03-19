dcdffffb968c596c50080a8e7b21f5fc8224c350
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-03-19 21:04:23 +0000

    Added explanation for Paris (Strand)

--- src/components/explanations/ParisStrand.vue
+++ src/components/explanations/ParisStrand.vue
@@ -13,7 +13,59 @@ import Attribution from "./Attribution.vue";
   <hr>
   <p class="text-center">Exhibited in 1815 at the Strand Panorama in London. The dimensions of this cyclorama are unknown, so the dimensions of the Lower Circle at the Leicester Square Panorama (which at the time was a competitor to the Strand Panorama) is used instead.</p>
   <hr>
-  <p>TODO</p>
+  <h1 class="text-center leading-4">
+    <span class="uppercase">Explanation</span><br>
+    <span class="uppercase text-xs tracking-widest">of</span><br>
+    <span class="italic">The <span class="uppercase tracking-widest">View</span> of the</span><br>
+    <span class="uppercase text-4xl font-medium">city of Paris,</span><br>
+    <span class="uppercase text-xs tracking-widest">now exhibiting in</span><br>
+    <span class="uppercase italic text-2xl">Barker'<span class="lowercase">s</span> Panorama,</span><br>
+    <span class="uppercase text-xl">Strand;</span><br>
+    <span style="font-variant: small-caps">Taken from the <span class="uppercase">Tuileries</span>.</span><br>
+  </h1>
+  <p class="text-center mb-[-1rem]">~~~~~</p>
+  <p class="text-center mb-[-1rem] italic">Open from Ten till Dusk.</p>
+  <p class="text-center mb-[-1rem]">~~~~~</p>
+  <p class="text-center text-xl">1815.</p>
+  <hr>
+  <ol>
+    <li>Dome and Roof of the Tuileries.</li>
+    <li>New Gallery of the Louvre.</li>
+    <li>Theatre Française.</li>
+    <li>Church of St. Fustache</li>
+    <li>Place Carrousel.</li>
+    <li>Triumphal Arch, in Commemoration of the Campaign of 1805.</li>
+    <li>King's Stables.</li>
+    <li>Barrack of the Hundred Swiss Guards who attended the King.</li>
+    <li>The Back of the Square of the Old Louvre.</li>
+    <li>The Gallery of Ancient Statues.</li>
+    <li>Gallery of Paintings.</li>
+    <li>The Ancient Part of the Palace of Justice.</li>
+    <li>Dome of the Modern Part of the Palace of Justice.</li>
+    <li>The Holy Chapel, built by St. Louis.</li>
+    <li>Cathedral of Notre Dame.</li>
+    <li>Palace of the Fine Arts.</li>
+    <li>The Bridge of Arts.</li>
+    <li>The Church of St. Genevieve.</li>
+    <li>L'Abbaye St. Germain.</li>
+    <li>Church of St. Suipice.</li>
+    <li>The House in which Voltaire lived.</li>
+    <li>La Pont Royale.</li>
+    <li>Two Floating-Baths.</li>
+    <li>Barrack of the Body Guard of the King.</li>
+    <li>Hospital of Invalids.</li>
+    <li>Chancelry of the Legion of Honour.</li>
+    <li>Hall of the Corps Legislative.</li>
+    <li>Pont de la Concord, or Pont Louis XV.</li>
+    <li>A new Triumphal Arch, erecting to the Glory of the Armies.</li>
+    <li>Garde Meuble de la Couronne.</li>
+    <li>Church of the Assumption</li>
+    <li>Column of the Grand Army.</li>
+    <li>Montmartre.</li>
+    <li>Church of St. Roche.</li>
+    <li>Gardens of the Tuileries.</li>
+  </ol>
+  <p><span style="font-variant: small-caps">J. Adlard,</span> Printer, 23, Bartholomew Close, Smithfield.</p>
 </template>
 
 <style scoped>
