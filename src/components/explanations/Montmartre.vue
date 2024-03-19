9c08a41c2b7065bd978997c5add2ff9a0c776011
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-03-19 20:35:19 +0000

    Added explanation for Montmartre

--- src/components/explanations/Montmartre.vue
+++ src/components/explanations/Montmartre.vue
@@ -13,7 +13,38 @@ import Attribution from "./Attribution.vue";
   <hr>
   <p class="text-center">Exhibited in 1814 at the Strand Panorama in London. The dimensions of this cyclorama are unknown, so the dimensions of the Lower Circle at the Leicester Square Panorama (which at the time was a competitor to the Strand Panorama) is used instead.</p>
   <hr>
-  <p>TODO</p>
+  <h1 class="text-center leading-4">
+    <span class="uppercase">Explanation</span><br>
+    <span class="uppercase text-xs tracking-widest">of</span><br>
+    <span class="italic">the <span class="uppercase tracking-widest">View</span> of</span><br>
+    <span class="uppercase text-5xl font-bold tracking-widest">Paris,</span><br>
+    <span class="uppercase text-xs tracking-widest">now exhibiting in</span><br>
+    <span class="uppercase italic text-2xl">Barker'<span class="lowercase">s</span> Panorama,</span><br>
+    <span class="uppercase text-xl">Strand;</span><br>
+    <span style="font-variant: small-caps">Taken from <span class="uppercase">Montmartre</span>.</span><br>
+  </h1>
+  <p class="text-center mb-[-1rem]">~~~~~</p>
+  <p class="text-center mb-[-1rem] italic">Open from Ten till Dusk.</p>
+  <p class="text-center mb-[-1rem]">~~~~~</p>
+  <p class="text-center text-xl">1814.</p>
+  <hr>
+  <ol>
+    <li>Telegraph on Montmartre.</li>
+    <li>Ground on which the Cossacks bivouacked.</li>
+    <li>Heights of Romainville and Belleville.</li>
+    <li>Pleasure Garden, called Au Poirier sans Pareil.</li>
+    <li>Cathedral Notre Dame.</li>
+    <li>Pantheon, formerly the Church of St. Genevieve.</li>
+    <li>Church of Vale de Grace.</li>
+    <li>Church of St. Sulpice.</li>
+    <li>Thuilleries.</li>
+    <li>Hospital of Invalids.</li>
+    <li>Military School.</li>
+    <li>Bridge of Jena and River Seine.</li>
+    <li>Palace and Village of St. Cloud.</li>
+    <li>Mount Valerian.</li>
+    <li>Road to St. Denys.</li>
+  </ol>
 </template>
 
 <style scoped>
