2f2bdebf30c584668edc77e2709a115872a732ee
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-03-19 19:51:30 +0000

    Added explanation for Vittoria

--- src/components/explanations/Vittoria.vue
+++ src/components/explanations/Vittoria.vue
@@ -13,9 +13,103 @@ import Attribution from "./Attribution.vue";
   <hr>
   <p class="text-center">Exhibited between July 1814 – April 29th, 1815 at Leicester Square in the <span class="whitespace-nowrap">Lower Circle.<a href="#citation1"><sup>[1]</sup></a></span></p>
   <hr>
-  <p>TODO</p>
+  <h1 class="text-center">
+    <span class="uppercase">Explanation</span><br>
+    <span class="uppercase text-xs">of the view of</span><br>
+    <span class="uppercase text-2xl italic">the splendid victory</span><br>
+    <span class="uppercase text-xs">Gained by</span><br>
+    <span class="">The <span class="uppercase">Marquis</span> of <span class="uppercase">Wellington</span></span><br>
+    <span class="italic">Over the <span class="uppercase">French Army</span>, consisting of 650,000 Men, commanded by <span class="uppercase">Joseph Bonaparte</span></span><br>
+    <span class="uppercase text-xs">on the</span><br>
+    <span class="uppercase text-3xl font-medium">Plains of Vittoria.</span><br>
+    <span class="text-sm">The View, painted by <span class="uppercase">Henry Aston Barker</span> and Assistants, from Drawings taken on the Spot by <span style="font-variant: small-caps">Mr. Burford</span>, is now exhibiting in the Great Rotunda of the</span><br>
+    <span class="uppercase text-4xl font-bold">Panorama, Leicester Square.</span><br>
+  </h1>
+  <hr>
+  <ol>
+    <li>Field Marshall the Marquis of <span style="font-variant: small-caps">Wellington</span>.</li>
+    <li>His Serene Highness the Hereditary Prince of orange.</li>
+    <li>Marshal Beresford.</li>
+    <li>Guerilla Officer.</li>
+    <li>Captain Fitz-Clarence.</li>
+    <li>Guerilla Officer.</li>
+    <li>Sir Richard Fletcher.</li>
+    <li>Sir Thomas Picton.</li>
+    <li>Village of Gardelague.</li>
+    <li>Portuguese Brigade in column.</li>
+    <li>General Anson's Brigade in ditto.—These two brigades, with Gen. Skerret's brigade, formed the 4th Division.</li>
+    <li>Village of Sarta.</li>
+    <li>Sir R. Hill's Corps, in columns, descending from the Heights of Puebla.</li>
+    <li>Village of Berostigruta<a href="#donibristle"><sup>[2]</sup></a></li>
+    <li>Two Battalions of the 95th Regt.</li>
+    <li>Gen. Fane's Brigade of heavy cavalry.</li>
+    <li>Castle of Zaldearan, the spot where Colonel Cadogan died.</li>
+    <li>Village of Armentia</li>
+    <li>Oxford Blues.</li>
+    <li>Village of Arinez.</li>
+    <li>Altura de los Inglezas, or English Hill.</li>
+    <li>The Villages of Margareta & Trespuentes are behind this House.</li>
+    <li>Life Guards.</li>
+    <li>Village of Zuago de Alava.</li>
+    <li>Major-general Inglis's Brigade of the 7th Division, and General Vandeleur's Brigade of the Light Division in columns.</li>
+    <li>Village of Estaronna.</li>
+    <li>Ditto of Ali. — General Ponsonby's Brigade of heavy cavalry is in front of the Wood.</li>
+    <li>Left Brigade of the 3rd Division.</li>
+    <li>Gran Pena de Gorbea Mountain.</li>
+    <li>Major Gardner's Troop of Horse Artillery.</li>
+    <li>Village of Abechuco.</li>
+    <li>Ditto of Arreaga.</li>
+    <li>Ditto of Retana.</li>
+    <li>Ditto of Gamarra Major.</li>
+    <li>Village of Gamarra Minor.</li>
+    <li>Ditto of Durana.</li>
+    <li>The French had two Divisions of Infantry in reserve upon this Hill.</li>
+    <li>The French Centre retreating in confusion.</li>
+    <li>Fifteenth Hussars charging the Enemy.</li>
+    <li>Road to Miranda.</li>
+    <li>Santa Maria Church.</li>
+    <li>San Miguel Ditto.</li>
+    <li>St. Francisco Hospital.</li>
+    <li>Villages of Lorriaga and Arcanta.</li>
+    <li>The French Left retreating towards Pamplona.</li>
+    <li>Village of Arechavelata.</li>
+    <li>Longas Guerillas descending from the Mountains.</li>
+  </ol>
+  <hr>
+  <p><span style="font-variant: small-caps">Lord Wellington's Staff</span> consisted of the following Officers</p>
+  <ul>
+    <li>Marshal Sir Wm. Beresford.</li>
+    <li>General Alava.</li>
+    <li>General Murrary, Quarter-Master General.</li>
+    <li>Lord Aylmer, Deputy-Adjutant-General.</li>
+    <li>Lord Fitzroy Somerset, Military Secretary.</li>
+    <li>Sir Richard Fletcher.</li>
+    <li>Hon. Lieut.-Col. Dundas, R. Staff Corps.</li>
+    <li>Lieut.-Col. Sturgeon, ditto.</li>
+    <li>Honorable Lieut. Col. Gordon. A. D. C.</li>
+    <li>His Serene Highness the Hereditary Prince of Orange, A. D. C.</li>
+    <li>Lieut.-Col. Campbell, Adj. Quarter-Master-General.</li>
+    <li>Lieut.-Col. Burgh, A. D. C.</li>
+    <li>Lieut.-Col. Dixon, Royal Artillery.</li>
+    <li>Major Canning, A. D. C.</li>
+    <li>Captain Freemantle, A. D. C.</li>
+    <li>Lieut.-Col. Broke, Adj.-Quarter-Master-General.</li>
+    <li>Lieut.-Col. Schovel, ditto.</li>
+    <li>Brigadier-Gen. O'Lawler, Spanish Service.</li>
+    <li>Captain Browne, Dep.-Assist.-Adj-Gen.</li>
+    <li>Captain Fitz Clarence, ditto.</li>
+    <li>Lt.-Col.Hervey, Portuguese Service.</li>
+    <li>Captain Don, Spanish Service.</li>
+    <li>Lieut.-Col. May, Royal Artillery.</li>
+    <li>Major Ellicombe, Royal Engineers.</li>
+    <li>Brigade-Major Woodeyar, Royal Artillery.</li>
+    <li>Lieut. Stanwag, Royal Engineers.</li>
+    <li>Lieut. Freeth, Deputy Adjutant-Quarter-Master-General.</li>
+  </ul>
+  <p class="italic">Lord Wellington and his Staff are painted by Mr. Atkinson.</p>
   <hr>
   <p id="citation1"><sup>[1]</sup> <em>The First Panoramas: Visions of British Imperialism</em> by Denise Oleksijczuk, p. 173</p>
+  <p id="berostigruta"><sup>[2]</sup> Today in English, Berrostegieta. In Spanish, Berrosteguieta. In Basque, Berroztegieta.</p>
 </template>
 
 <style scoped>
