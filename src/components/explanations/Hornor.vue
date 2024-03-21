fdc7dc7a1c5760c20cac6902ca5d046987ccbac5
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-03-21 22:03:23 +0000

    Minor text change + Completed Hornor text

--- src/components/explanations/Hornor.vue
+++ src/components/explanations/Hornor.vue
@@ -25,6 +25,271 @@ import Attribution from "./Attribution.vue";
   <hr>
   <p class="text-center">[Plate 1]</p>
   <ol>
+    <li>Wimbledon, Surrey</li>
+    <li>Wimbledon Park</li>
+    <li>Putney.</li>
+    <li>Fulham.</li>
+    <li>Mortlake & Barnes.</li>
+    <li>Richmond Hill.</li>
+    <li>Chiswick & Hammersmith.</li>
+    <li>Pagoda, Kew Gardens.</li>
+    <li>Acton, Middlesex.</li>
+    <li>Vauxhall Bridge</li>
+    <li>Penitentiary Milbank</li>
+    <li>Wandsworth.</li>
+    <li>Battersea</li>
+    <li>Chelsea new Church</li>
+    <li>Hyde Park</li>
+    <li>Lambeth Palace</li>
+    <li>St. John's Church, Westminster</li>
+    <li>Westminster Hall</li>
+    <li>Westminster Abbey</li>
+    <li>Chelsea Hospital.</li>
+    <li>War Office, Horse Guards.</li>
+    <li>Buckingham Palace.</li>
+    <li>St. James Park</li>
+    <li>Italian Opera Ho. Hay Market.</li>
+    <li>St. James Church, Piccadilly.</li>
+    <li>St. Ann's Church Soho.</li>
+    <li>Westminster Bridge.</li>
+    <li>St. Martin in the Fields.</li>
+    <li>St. Paul, Covent Garden.</li>
+    <li>Lambeth new Chh. W. B. Road.</li>
+    <li>Waterloo Bridge.</li>
+    <li>Adolphi Terrace.</li>
+    <li>Somerset House.</li>
+    <li>St. Mary le Strand</li>
+    <li>Albion Place Bkfrs. Bridge.</li>
+    <li>Blackfriars Bridge</li>
+    <li>The Temple.</li>
+    <li>St. Clement's Church Strand</li>
+    <li>Earl Street Blackfriars.</li>
+    <li>Bridewell Hall Bridge Str</li>
+    <li>St. Andrew's Wardrobe.</li>
+    <li>Printers Square</li>
+    <li>Union Street Blackfriars</li>
+    <li>Wardrobe Palace.</li>
+    <li>St. Andrew's Hill</li>
+  </ol>
+  <p class="text-center">[Plate 2]</p>
+  <ol>
+    <li>Grand Junction Reserve Bayswab</li>
+    <li>Grand ditto Canal.</li>
+    <li>Harrow on the Hill, Middx.</li>
+    <li>Portland Town Regents Park.</li>
+    <li>Primrose Hill.</li>
+    <li>Hampstead Church</li>
+    <li>Hampstead Heath.</li>
+    <li>Oxford Street</li>
+    <li>Regent Street.</li>
+    <li>All soul's Ch. Langham Place.</li>
+    <li>St. Mary le bene new Church.</li>
+    <li>Regents Park</li>
+    <li>The Colosseum, Rs. Park</li>
+    <li>Gothic Chapel Rs. park</li>
+    <li>Camden Town</li>
+    <li>Long Acre</li>
+    <li>St. Giles in the Fields.</li>
+    <li>St. George's Blomsbury</li>
+    <li>British Museum</li>
+    <li>Russell Square.</li>
+    <li>St. Pancras new Church</li>
+    <li>New Church, Sommers Town.</li>
+    <li>New Chapel, Camden Town</li>
+    <li>St. Clements Ch. Strand.</li>
+    <li>St. Dunstan Fleet Street.</li>
+    <li>Chancery Lane.</li>
+    <li>Lincolns Inn Fields</li>
+    <li>Chapel Lincolns Inn</li>
+    <li>Stone Buildings ??</li>
+    <li>Holborn</li>
+    <li>Queen's Square.</li>
+    <li>Lambs Conduit Street.</li>
+    <li>Gray's Inn</li>
+    <li>Guildford Street</li>
+    <li>Burton Crescent.</li>
+    <li>The Foundling Hospital</li>
+    <li>New Church Mecklenburg Sq.</li>
+    <li>Small Pox Hospital.</li>
+    <li>Temple Ch. in the Temple</li>
+    <li>Fleet Street</li>
+    <li>Rolls Gardens</li>
+    <li>Little New Street</li>
+    <li>Thaives Inn</li>
+    <li>St. Andrew's Holborn</li>
+    <li>Furniva's Inn</li>
+    <li>Ely Chapel, Ely Place.</li>
+    <li>Brewery, Liquorpond Street</li>
+    <li>St. Brides Fleet Street</li>
+    <li>Bridge Street, Blackfriars</li>
+    <li>Fleet Market</li>
+    <li>Skinner St. Snow Hill</li>
+    <li>St. Sepulchres Ch. do.</li>
+    <li>Little Bridge Street</li>
+    <li>The Fleet Prison</li>
+    <li>Seacoal Lane.</li>
+    <li>Fleet Lane</li>
+    <li>Broad Way</li>
+    <li>St. Martins Ludgate Hill</li>
+    <li>Newgate Prison</li>
+    <li>College of Physicians Warw ???</li>
+    <li>Stationers Hall</li>
+    <li>Amen Corner.</li>
+  </ol>
+  <p class="text-center">[Plate 3]</p>
+  <ol>
+    <li>Highgate Terrace</li>
+    <li>do. Archway</li>
+    <li>Kentish Town</li>
+    <li>Holloway new Church</li>
+    <li>Holloway Chapel</li>
+    <li>Islington Church</li>
+    <li>Gasometer, Battle Bridge</li>
+    <li>Bellisle</li>
+    <li>New Church Pentonville</li>
+    <li>St. James' Chapel, do.</li>
+    <li>New Church Clarendon Sq. Pentonville</li>
+    <li>Angel Inn, Islington</li>
+    <li>Cold Bath Fields Prison</li>
+    <li>Guldford Place</li>
+    <li>Spa fields Chapel</li>
+    <li>New River Reservoir</li>
+    <li>Northampton Square</li>
+    <li>Goswel Street Road</li>
+    <li>New Church, St. Lukes</li>
+    <li>Gl. Saffron Hill</li>
+    <li>Coppice Row</li>
+    <li>Sessions House, Clerkenwell</li>
+    <li>St. James, Clerkenwell</li>
+    <li>St. John's Sq. do.</li>
+    <li>St. John's Ch. do.</li>
+    <li>St. John Street</li>
+    <li>Charter House</li>
+    <li>Goswell Street</li>
+    <li>Chick lane</li>
+    <li>Turnmill St.</li>
+    <li>Red Lion Street.</li>
+    <li>St. John's Gate, Clerkenwell</li>
+    <li>St. Bartholomew the Great</li>
+    <li>Long Lane</li>
+    <li>St. Sepulchre's, Snow Hill</li>
+    <li>Giltspur Street</li>
+    <li>St. Batholomew the Less</li>
+    <li>Smithfield</li>
+    <li>Duke Street</li>
+    <li>Bartholomew Close</li>
+    <li>Giltspur St. Counter</li>
+    <li>New Halt of Christ's Hospital</li>
+    <li>St. Bartholomews Hospital</li>
+    <li>Little Britain</li>
+    <li>Newgate Street</li>
+    <li>Christ's Hospital</li>
+    <li>Christ. Church</li>
+    <li>Bagnio Court</li>
+    <li>Phoenix Court</li>
+    <li>Rose Street</li>
+    <li>Dolley's Chop House</li>
+  </ol>
+  <p class="text-center">[Plate 4]</p>
+  <ol>
+    <li>The New River</li>
+    <li>Old Church Stoke Newington</li>
+    <li>New do. do.</li>
+    <li>Hackney Church</li>
+    <li>Jews' Chapel & School Bethnall Gn.</li>
+    <li>New Church Stoke Green.</li>
+    <li>New Church Haxton.</li>
+    <li>Kingsland Road.</li>
+    <li>Haggerstone New Churh</li>
+    <li>St. Leonard Shoreditch</li>
+    <li>St. Matthew, Bethnall Green</li>
+    <li>Ironmonger Row, St. Lukes.</li>
+    <li>St. Lukes Ch. Old Street.</li>
+    <li>St. Lukes Hospital.</li>
+    <li>Lying In do.</li>
+    <li>Artillery House</li>
+    <li>Finsbury Square</li>
+    <li>Bishopsgate Street.</li>
+    <li>Aldersgate Street.</li>
+    <li>Barbican</li>
+    <li>Jewin Crescent.</li>
+    <li>Golden Lane Brewery.</li>
+    <li>Chapel, Grub Street.</li>
+    <li>Finsbury Place.</li>
+    <li>London Institution, Moor Fields.</li>
+    <li>Caledonian Chapel, London Wall.</li>
+    <li>St. Botolph, Aldersgate St.</li>
+    <li>St. Anns Ch. St. Ann's Lane</li>
+    <li>Foster Lane</li>
+    <li>St. Giles, Cripplegate.</li>
+    <li>Addle Street.</li>
+    <li>Wood Street.</li>
+    <li>St. Alban's, Wood Street.</li>
+    <li>St. Mary, Aldermanbury.</li>
+    <li>St. Michael Basinghall St.</li>
+    <li>Guild Hall.</li>
+    <li>See Newgate Street.</li>
+    <li>St. Martin le Grand</li>
+    <li>New Post Office.</li>
+    <li>Goldsmith's Hall</li>
+    <li>St. Michael, Wood Street.</li>
+    <li>Aldermanbury.</li>
+    <li>St. Vedast Foster Lane.</li>
+    <li>See Foster Lane.</li>
+  </ol>
+  <p class="text-center">[Plate 5]</p>
+  <ol>
+    <li>Stratford Essex</li>
+    <li>Bow Mile End</li>
+    <li>Limehouse Church.</li>
+    <li>Poplar Church.</li>
+    <li>Mast House, Blackwall</li>
+    <li>Woolwich, Kent.</li>
+    <li>Model Repository, Wollwich.</li>
+    <li>New Church, Globs Town.</li>
+    <li>Christ Church, Spitalfields.</li>
+    <li>Gothic Chapel, Stepney</li>
+    <li>Stepney Church</li>
+    <li>Commeroial Road</li>
+    <li>West India Docks</li>
+    <li>City Canal</li>
+    <li>Isle of Dogs</li>
+    <li>St. Botoplh, Bishopsgate St.</li>
+    <li>St. Ann, Aldgate.</li>
+    <li>White chapel Church.</li>
+    <li>Goodmans Fields.</li>
+    <li>St. Paul, Shadwell.</li>
+    <li>St. George in the East</li>
+    <li>London Docks.</li>
+    <li>Royal Mint, Tower Hill.</li>
+    <li>Moore Fields</li>
+    <li>Brokers Row.</li>
+    <li>Allhallows, London Wall</li>
+    <li>St. Peters, Broad Street.</li>
+    <li>St. Stephen, Coleman Street</li>
+    <li>Bank of England.</li>
+    <li>St. Bennet</li>
+    <li>St. Bartholomew.</li>
+    <li>Royal Exchange.</li>
+    <li>St. Michael Cornhill.</li>
+    <li>St. Peters.</li>
+    <li>Allhallows, Gracechurch St.</li>
+    <li>St. Edmund, Lombard St.</li>
+    <li>St. Dienis, Fenchurch St.</li>
+    <li>St. Bennet do.</li>
+    <li>St. Olave, crutcker Friars.</li>
+    <li>East India Warehouses</li>
+    <li>St. Lawrence Jewry.</li>
+    <li>St. Stephens, Coleman St.</li>
+    <li>St. Olave, Old Jewry.</li>
+    <li>Bow Church, Cheapside.</li>
+    <li>St. Mildred, Poultry.</li>
+    <li>The Mansion House.</li>
+    <li>St. Stephen, Walbrook.</li>
+    <li>St. Matthew, Friday Street.</li>
+  </ol>
+  <p class="text-center">[Plate 6]</p>
+  <ol>
     <li>Cadet College Woolwich</li>
     <li>Charlton Church.</li>
     <li>Tower on Shooters Hill.</li>
@@ -58,7 +323,52 @@ import Attribution from "./Attribution.vue";
     <li>St. Mildred, Bread St.</li>
     <li>St. Austin, Watling Street.</li>
   </ol>
-  <p class="text-center">[Plate 2]</p>
+  <p class="text-center">[Plate 7]</p>
+  <ol>
+    <li>Bridge on the Surrey Canl. Kent Rd.</li>
+    <li>Chapel, Peckham</li>
+    <li>Spire of Beckenham Ch. Kent.</li>
+    <li>Dulwich Woods.</li>
+    <li>Camberwell, new Church.</li>
+    <li>St. Peter's, Walworth.</li>
+    <li>St. Saviour, Southwark.</li>
+    <li>St. Thomas', Southwark.</li>
+    <li>Bermondsey Church.</li>
+    <li>St. George, High St. Boro</li>
+    <li>New Church Dover Road</li>
+    <li>Potts Vinegar Manufactory</li>
+    <li>Union Street</li>
+    <li>Southwark Bridge</li>
+    <li>St. Michael, Thames St.</li>
+    <li>St. Mary, Thames Street</li>
+    <li>St. Nicholas, Fish Street.</li>
+    <li>St. Mary, ditto</li>
+  </ol>
+  <p class="text-center">[Plate 8]</p>
+  <ol>
+    <li>Norwood new Church.</li>
+    <li>Streatham, Surrey.</li>
+    <li>South Brixton Church</li>
+    <li>Brixton Jail</li>
+    <li>Elephant & Castle, Newington.</li>
+    <li>Newington Church</li>
+    <li>Kennington Church</li>
+    <li>Clapham</li>
+    <li>Kings Bench Prison.</li>
+    <li>Surrey Theatre, Gt. Surrey St.</li>
+    <li>New Bethlehem Hospi.</li>
+    <li>Vauxhall Bridge.</li>
+    <li>Union Street. Borough.</li>
+    <li>Nelson Sq. Gt. Surrey St.</li>
+    <li>Rowland Hills Chapel</li>
+    <li>Great Surrey Street.</li>
+    <li>Christ Ch do.</li>
+    <li>Coburgh Theatre.</li>
+    <li>Holland St. Bankside</li>
+    <li>Back of Albion Place.</li>
+    <li>St. Bennet, Thames Street.</li>
+    <li>Doctor's Commons.</li>
+  </ol>
 </template>
 
 <style scoped>
