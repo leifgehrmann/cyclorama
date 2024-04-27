bfb54485b8f80029ab979ff2cc85e1522cb95692
Leif Gehrmann <leif.gehrmann@gmail.com>
2024-04-27 21:46:24 +0100

    Added details about London exhibition

--- src/components/explanations/LondonBarker.vue
+++ src/components/explanations/LondonBarker.vue
@@ -22,15 +22,16 @@ const highQuality = urlParams.get('hq') === 'true';
   </p>
   <p class="text-center text-xl">1792-1793</p>
   <hr>
-  <p>The original panorama no longer exists, but this aquatint is possibly a recreation. A panorama from Albion Mills was first exhibited at 28 Castle Street in London in June 11th 1791 until early 1794.<a href="#citation1"><sup>[1]</sup></a> It was small and not a complete circle.<a href="#citation2"><sup>[2]</sup></a> It was later exhibited at Leicester Square between March 28th, 1795 and February 13th, 1796. It's unclear how different these panoramas were from the aquatints.</p>
+  <p>The original panorama no longer exists, but this aquatint is possibly a recreation. A panorama from Albion Mills was first exhibited at 28 Castle Street in London in June 11th 1791 until early 1794.<a href="#citation1"><sup>[1]</sup></a> It was small and not a complete circle.<a href="#citation2"><sup>[2]</sup></a> It was later repainted to be 360-degress and exhibited at Leicester Square between March 28th, 1795 and February 13th, 1796. Finally it put on tour across Europe, exhibiting in Hamburg, Leipzig, Prague, Vienna, and Paris.<a href="#citation3"><sup>[3]</sup></a> It's unclear how different these panoramas were from the aquatints.</p>
   <p>The dimensions of this simulated cyclorama are not the same as the dimensions of the original cyclorama; the original had a much smaller diameter. At 28 Castle Street the dimensions are unknown. At Leceister Square the diameter of the Upper Circle was 50 feet (15 meters). This simulation uses the dimensions of the Lower Circle at Leicester Square, which has a diameter of 84 feet (25 meters), to demonstrate Robert Barker's desired vision.</p>
-  <p>Ralph Hyde noted in his book <em>Panoramania</em> some nice details.<a href="#citation3"><sup>[3]</sup></a> "At the S. end of Blackfriars Bridge workmen repair the road, a waterman descends the stairs to his vessel, a man raps on a door of Albion Place, and a woman calls out of a window. River craft close by includes a barge of the Royal Exchange Fire Office, an allusion to the fire, fought by insurance fire boats, which gutted the Albion Mills between the completion of Barker's drawings and the publication of the prints."</p>
+  <p>Ralph Hyde noted in his book <em>Panoramania</em> some nice details.<a href="#citation4"><sup>[4]</sup></a> "At the [south] end of Blackfriars Bridge workmen repair the road, a waterman descends the stairs to his vessel, a man raps on a door of Albion Place, and a woman calls out of a window. River craft close by includes a barge of the Royal Exchange Fire Office, an allusion to the fire, fought by insurance fire boats, which gutted the Albion Mills between the completion of Barker's drawings and the publication of the prints."</p>
   <p>A key of the panorama can be found at the <a href="https://www.britishmuseum.org/collection/object/P_1880-1113-5509-1">British Museum</a> (Museum Number: 1880,1113.5509.1)</p>
   <p v-if="!highQuality">For performance reasons, the image quality of this panorama was reduced so the experience on mobile phones is smoother. <a href="./?view=albionMills&hq=true">Click here to view a higher quality version.</a></p>
   <hr>
   <p id="citation1"><sup>[1]</sup> <em>The First Panoramas: Visions of British Imperialism</em> by Denise Oleksijczuk, p. 48</p>
   <p id="citation2"><sup>[2]</sup> <em>Panoramania: The Art and Entertainment of the "all-embracing" View</em> by Ralph Hyde, p. 62</p>
-  <p id="citation3"><sup>[3]</sup> Ibid, p. 64</p>
+  <p id="citation2"><sup>[3]</sup> <em>'Spectacles within doors': Panoramas of London in the 1790s</em> by Markman Elles</p>
+  <p id="citation2"><sup>[4]</sup> <em>Panoramania: The Art and Entertainment of the "all-embracing" View</em> by Ralph Hyde, p. 64</p>
 </template>
 
 <style scoped>
